098dfda62cf29b7e19a22ea4d45a9ee5c84e54ce
Karl Ramberg <karlramberg@protonmail.com>
2021-07-11 14:26:10 -0500

    Re-work structure for pure HTML

--- websiteBuilder.go
+++ websiteBuilder.go
@@ -1,3 +1,9 @@
+/*
+   Website Builder
+   Author: Karl Ramberg
+   Last modified: 2021-07-11
+*/
+
 package main
 
 import (
@@ -9,46 +15,52 @@ import (
 )
 
 func main() {
-	// Exit if 4 arguments are not given
-	if len(os.Args) < 5 {
-		fmt.Println("USAGE: $ website_builder <input folder> <output folder> <template file> <replace token>")
+	// Exit if 5 arguments are not given
+	if len(os.Args) != 5 {
+		fmt.Println("USAGE: $ website_builder <input folder> <output folder> <template> <token>")
 		return
 	}
 
 	inputFolder := os.Args[1]
 	outputFolder := os.Args[2]
 
-	// Read the HTML template file into a string
+	// Read template into a string
 	template, err := ioutil.ReadFile(os.Args[3])
 	if err != nil {
-		fmt.Println("ERROR: Template file not found or is protected")
+		fmt.Println("ERROR: Template not found or is protected")
 		return
 	}
 
-	replaceToken := os.Args[4]
+	token := os.Args[4]
 
-	// Walk through the root directory file by file
+	// Walk through the input folder file by file
 	filepath.Walk(inputFolder, func(path string, info os.FileInfo, err error) error {
-		// Create mirrored folder structure
+		// If a folder is found, mirror it in the output folder 
 		if info.IsDir() {
 			newFolder := outputFolder + path[len(inputFolder):]
 			os.MkdirAll(newFolder, 0755)
 			fmt.Println("Generating " + newFolder + "...")
-			return nil
 		}
 
-		// Grab contents from any *.html file
-		newFile := outputFolder + path[len(inputFolder):len(path)-len(filepath.Ext(path))] + ".html"
-		fmt.Println("Generating " + newFile + "...")
-		content, err := ioutil.ReadFile(path)
-		if err != nil {
-			fmt.Println("ERROR: Cannot access " + path)
-			return nil
+		// If an HTML file is found, generate a completed one
+		// and write it to the same location in the output folder
+		if filepath.Ext(path) == ".html" {
+			// Read in contents of input file
+			content, err := ioutil.ReadFile(path)
+			if err != nil {
+				fmt.Println("ERROR: Cannot access " + path)
+				return nil
+			}
+
+			// Replace the token with content
+			output := strings.Replace(string(template), token, string(content), 1)
+
+			// Genearate the output file's path and write to disk
+			newFile := outputFolder + path[len(inputFolder):]
+			fmt.Println("Generating " + newFile + "...")
+			ioutil.WriteFile(newFile, []byte(output), 0644)
 		}
 
-		// Replace the token with HTML
-		output := strings.Replace(string(template), replaceToken, string(content), 1)
-		ioutil.WriteFile(newFile, []byte(output), 0644)
 		return nil
 	})
 }
